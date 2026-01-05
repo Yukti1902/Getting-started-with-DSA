8d2608e39557142a819dd215085036c867050ddd
Yukti Gupta <149217253+Yukti1902@users.noreply.github.com>
2026-01-05 17:38:41 +0530

    Enhance array operations with additional examples
    Added examples for array copying, comparison, conversion, and frequency counting.

--- Arrays/Arrays_Basic_II.java
+++ Arrays/Arrays_Basic_II.java
@@ -28,10 +28,43 @@ public class Arrays_Basics_II {
             mer[arr.length + i] = arrb[i]; // Copying elements of second array
         }
         System.out.println("Merged Array:" + Arrays.toString(mer));
-        
 
+        //Copying an array (System.arraycopy, clone, Arrays.copyOf)
+        int org[] = {10,20,30,40,50};
+        int copy[] = new int[org.length];
+        System.arraycopy(org,0,copy,0,org.length);
+        System.out.println("Original Array: " + Arrays.toString(org));
+        System.out.println("Copied Array: " + Arrays.toString(copy));
+
+        //Comparing two arrays (Arrays.equals)
+        int arrone[] = {1,2,3,4,5};
+        int arrtwo[] = {1,2,3};
+        System.out.println("Are the arrays equal?" + Arrays.equals(arrone,arrtwo));
+
+        //Converting array to String (Arrays.toString)
+        int nums[] = {10,23,45,67};
+        System.out.println("ARRAY: " + Arrays.toString(nums));
+
+        //Converting string to array (split, toCharArray)
+        String str = "Hello we are writing the code in java";
+        String words[] = str.split(" ");
+        System.out.println("Array of String: " + Arrays.toString(words));
+        char chr[] = str.toCharArray();
+        System.out.println("Array of Characters: " + Arrays.toString(chr));
+
+        //Frequency of elements (basic HashMap / loop)
+        int freq[] = {2,3,4,4,7,2,5,7,8};
+        int count = 0;
+        for(int i=0; i<freq.length; i++){
+            count = 0;
+            for(int j =0; j<freq.length; j++){
+                if(freq[i] == freq[j]){
+                    count++;
+                }
+            }
+            System.out.println("Frequency of " + freq[i] + "is" + count);
+        }
         }
 
 
     }
-
