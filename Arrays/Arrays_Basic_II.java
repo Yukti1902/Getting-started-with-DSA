283e85442acc75746d5aaf95c83c2a37170356ad
Yukti Gupta <149217253+Yukti1902@users.noreply.github.com>
2026-01-19 23:46:03 +0530

    Update Arrays_Basic_II.java

--- Arrays/Arrays_Basic_II.java
+++ Arrays/Arrays_Basic_II.java
@@ -59,7 +59,7 @@ public class Arrays_Basics_II {
             count = 0;
             for(int j =0; j<freq.length; j++){
                 if(freq[i] == freq[j]){
-                    count++; //incrementing count
+                    count++; 
                 } 
             }
             System.out.println("Frequency of " + freq[i] + "is" + count);
